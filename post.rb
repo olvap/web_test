72140fa4c90206df302b238fc3eae5e81f5d4e7c
Pablo oldani <oldani.pablo@gmail.com>
2012-10-17 17:29:31 -0300

    ahora si responde

--- post.rb
+++ post.rb
@@ -5,6 +5,6 @@ video = {:Base64Data => File.new('video.mov'), :Base64Thumbnail => "asdsad"}
 puts video
 
 res = RestClient.post('http://50.112.145.70:3000/api/video/getVideoURL.json', 
-  'params1'=>params, "params" => video)
+  'params'=>params)
 
 puts res.body
